7c51d42e2299e750cc8e14081073d7ce9d0ee788
hplegend <hp.he@qunar.com>
2020-12-04 15:44:06 +0800

    m

--- meaf_sift_seg.py
+++ meaf_sift_seg.py
@@ -9,6 +9,7 @@ from matplotlib import pyplot as plt
 
 
 def cacl_hsv_hist(image_data):
+	img_height, img_width, _ = image_data.shape
 	hsv_roi = cv2.cvtColor(image_data, cv2.COLOR_BGR2HSV)
 	# isu.show_image_with_fix_windows(hsv_roi)
 
@@ -28,7 +29,7 @@ def cacl_hsv_hist(image_data):
 	# 过滤
 	# 相当于阈值分割那么一回事
 	# mask 已经是灰度图了
-	mask = cv2.inRange(hsv_roi, np.array((0., 40., 32.)), np.array((180., 255., 255.)))
+	mask = cv2.inRange(hsv_roi, np.array((0., 30., 32.)), np.array((180., 255., 255.)))
 	isu.show_image_with_fix_windows(mask, False)
 
 	roi_hist = cv2.calcHist([hsv_roi], [0], None, [180], [0, 180])
@@ -61,17 +62,50 @@ def cacl_hsv_hist(image_data):
 	# 选择大概率的，然后进行反向投影
 	# dst 应该是概率图
 	new_raw_image = cv2.imread('./images/crop/IMG_20181003_140313.jpg', -1)
-	hsv_roi_test = cv2.cvtColor(new_raw_image, cv2.COLOR_BGR2HSV)
+	hsv_roi_test = cv2.cvtColor(image_data, cv2.COLOR_BGR2HSV)
 
 	# 反向投影是基于概率的
 	dst = cv2.calcBackProject([hsv_roi_test], [0], roi_hist, [0, 180], 1)
+	blur_ret = cv2.blur(dst, (3, 3))
+	median_ret = cv2.medianBlur(blur_ret, 3)
 
-	isu.show_image_with_fix_windows(dst, False)
+	# isu.show_image_with_fix_windows(dst, False)
+	isu.show_image_with_fix_windows(median_ret, False)
 
-	plt.plot(roi_hist, label='B', color='blue')
-	plt.show()
-	pass
+	# 开始用反向投影作为模板抠图
+	empty_new_image = np.zeros((img_height, img_width, 3), dtype='uint8') + 255
+	# mask_inv = cv2.bitwise_not(maskImage)
+	extract_mask_image = cv2.bitwise_and(image_data, empty_new_image, mask=blur_ret)
 
+	# 剔除小轮廓
+	gray = cv2.cvtColor(extract_mask_image, cv2.COLOR_BGR2GRAY)
+	ret, thresh = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY_INV)
+
+	isu.show_image_with_fix_windows(thresh, False)
+
+	_, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+	reserve_contours = []
+
+	for ele_contour in contours:
+		area = cv2.contourArea(ele_contour)
+		if area > 0 and area < 50:
+			continue
+		if area > img_width * img_height * 0.8:
+			continue
+
+		reserve_contours.append(ele_contour)
+
+	print(len(reserve_contours))
+
+	reserve_contour_image = np.zeros((img_height, img_width, 3), dtype='uint8') + 0
+	cv2.drawContours(reserve_contour_image, reserve_contours, -1, (255, 255, 255), thickness=-1)
+	isu.show_image_with_fix_windows(reserve_contour_image, True)
+
+	isu.show_image_with_fix_windows(extract_mask_image, True)
+
+
+pass
 
 files = os.listdir('./images/crop')
 
